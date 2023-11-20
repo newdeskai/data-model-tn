e0dd8fd4998294287b367dbb4a9809155c9587b0
odooTN <151531194+odooTN@users.noreply.github.com>
2023-11-20 18:18:30 +0530

    Add files via upload

---
+++ data-model-tn/INSTALLATION MANUAL/ReadMe.txt
@@ -0,0 +1,18 @@
+
+------------------------------------------------------------------------------
+We are a trusted source for Odoo products and provide the best products but directly through our website https://techneith.com/
+
+If you haven't bought the Odoo Data Model by Techneith yet
+
+visit -->https://techneith.com/products/odoo-data-model/
+
+where you do not have to pay for extra charges from any app store
+
+-------------------------------------------------------------------------------
+
+To see Installation guide of Odoo Data Model 
+
+visit --> https://techneith.com/resources/data-model-guide/
+
+
+-------------------------------------------------------------------------------
---
+++ data-model-tn/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Odoo Data Model",
+
+    'summary': """
+       	    Odoo data model shows table relationship diagram of a model with other models 
+    (Odoo Data Tables), click on the model and see Entity Relationship Diagram (ER) ! relation table 
+    relationship 
+    table Power BI or PowerBI table chart table view tables graph table graph tables chart 
+    sql query sql exucution sql query execution tableau tablu tablue tableu""",
+
+    'description': """
+    Odoo data model shows table relationship diagram of a model with other models 
+    (Odoo Data Tables), click on the model and see Entity Relationship Diagram (ER) ! relation table 
+    relationship table Power BI or PowerBI table chart table view tables graph table graph tables chart 
+    sql query sql exucution sql query execution tableau tablu tablue tableu""",
+         'live_test_url': 'https://techneith.com/products/odoo-data-model/',
+    'maintainer': 'Entity Diagram',
+    'author': "Entity Diagram",
+    'website': "https://techneith.com/products/odoo-data-model/",
+    'support': "homerobotic21@gmail.com",
+ 
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '5.9',
+    'price': 0,
+    'currency': 'USD',
+    # any module necessary for this one to work correctly
+ 
+    'images': ['static/description/dataModel.gif'],
+    "license":"LGPL-3",
+
+    # always loaded
+   
+    # only loaded in demonstration mode
+    "application": True,
+    "installable": True,
+     'data': [
+
+    ],
+   'depends': [],
+}
